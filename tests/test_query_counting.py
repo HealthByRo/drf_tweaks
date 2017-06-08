12f289579b9c15d58a2c5095fa7a17db40542680
Michal Proszek <michal.proszek@gmail.com>
2017-06-08 15:08:55 +0200

    [BE] APD-350 - move query counter in tests to DRF Tweaks

---
+++ tests/test_query_counting.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import warnings
+
+from django.urls import reverse_lazy
+from django.conf.urls import url
+from django.http import HttpResponse
+from django.test import override_settings
+from rest_framework.test import APITestCase
+
+from drf_tweaks import test_utils
+from tests.models import SampleModel
+
+
+def custom_view(request):
+    SampleModel.objects.create(a="a", b="b")
+    return HttpResponse()
+
+
+def many_calls_view(request, n):
+    for i in range(int(n)):
+        SampleModel.objects.create(a="a", b="b")
+    return HttpResponse()
+
+
+urlpatterns = [
+    url(r"^sample/$", custom_view, name="sample"),
+    url(r"^calls/(?P<n>[0-9]+)/$", many_calls_view, name="calls")
+]
+
+
+class TestQueryCounter(APITestCase):
+    @override_settings(ROOT_URLCONF="tests.test_query_counting", TEST_QUERY_NUMBER_SHOW_WARNING=2,
+                       TEST_QUERY_NUMBER_RAISE_ERROR=3)
+    def test_query_counting_client(self):
+        client = test_utils.QueryCountingAPIClient()
+        for method in ("get", "post", "put", "patch"):
+            getattr(client, method)(reverse_lazy("sample"))
+            self.assertEqual(test_utils.test_query_counter, 1)
+
+        # test sending warning
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            client.post(reverse_lazy("calls", kwargs={"n": 3}))
+            self.assertEqual(len(w), 1)
+
+        # test raising error
+        with self.assertRaises(Exception):
+            client.post(reverse_lazy("calls", kwargs={"n": 4}))
