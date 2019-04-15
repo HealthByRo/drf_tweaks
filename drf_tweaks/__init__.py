72eec128178a085b1eace997412683f0a85f48a7
Michal Proszek <michal.proszek@gmail.com>
2019-04-15 11:32:47 +0200

    fix lock limiter

--- drf_tweaks/__init__.py
+++ drf_tweaks/__init__.py
@@ -1,8 +1,8 @@
 __title__ = "DRF Tweaks"
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 __author__ = "Pawel Krzyzaniak"
 __license__ = "MIT"
-__copyright__ = "Copyright 2017 Arabella"
+__copyright__ = "Copyright 2019 Arabella"
 
 # Version synonym
 VERSION = __version__
