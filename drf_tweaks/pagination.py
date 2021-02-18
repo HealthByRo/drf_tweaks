5656d55809405bd5dff9e4cbe6f551e3ebbde993
Michał Bielawski <michal.bielawski@apperio.com>
2021-02-18 17:05:14 +0000

    Use gettext_lazy instead of ugettext_lazy

--- drf_tweaks/pagination.py
+++ drf_tweaks/pagination.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from collections import OrderedDict
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
+
 from rest_framework import status
 from rest_framework.exceptions import APIException
 from rest_framework.pagination import (LimitOffsetPagination, NotFound, PageNumberPagination, remove_query_param,
--- drf_tweaks/versioning.py
+++ drf_tweaks/versioning.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from rest_framework import status
 from rest_framework.exceptions import APIException
 
